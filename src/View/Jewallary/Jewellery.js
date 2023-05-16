ae6bd7aff049c9bf450b8981092ef31949cf34ce
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2023-05-16 23:10:34 +0530

    folder structure updated

--- src/View/Jewallary/Jewellery.js
+++ src/View/Jewallary/Jewellery.js
@@ -3,13 +3,30 @@ import Navbar from '../../components/Navbar/Navbar'
 import './jewellery.css'
 function Jewellery() {
   return (
+  
     <>
     <Navbar/>
-  <div className='container-banner bg-dark'>
+    <div className='poster-container bg-dark'>
+  <h1 className='shop-name'>name of the shop</h1>
+                  <p className='blockquote-footer dis-sty'>discription about shop</p>
+   <div className='part-cont'>
+          <div className='part-1'>
+      <img src='./image\jewellary-img\png2.png' className='png-sty'/>
+      </div>
+      <div className='part-2'>
+        <p>containt</p>
+      </div>
+      <div className='part-3'>
     
-  </div> 
-      </>
+      </div>
+   </div>
+  <div className='address-cont'>
+    <span>talmale layout butibori nagpur<br/>nagpur 441108</span>
+  </div>
+  </div>
+ 
+    </>
   )
 }
 
-export default Jewellery
+export default Jewellery;
